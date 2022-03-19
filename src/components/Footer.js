b618b198fec594500445f1c465af393a0ef0a596
vrrock <63670007+vrrock@users.noreply.github.com>
2022-03-19 14:22:49 -0400

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css';
+
+export default function Footer() {
+    return (
+      <div className="footer">
+        <section className="footer-links content-container">
+          <div className="text-center p-4">
+            <a className="text-reset fw-bold" href="https://github.com/AkhileshThite/dtube/">GitHub</a>
+          </div>
+        </section>
+      </div>
+    );
+}
